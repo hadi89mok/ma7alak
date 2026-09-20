1b074d33fdadfe8c6daf912f18ba7dfd6edf1f66
hadi89mok <119508905+hadi89mok@users.noreply.github.com>
2026-09-20 14:43:20 +0300

    Bump ShoufHon service worker cache

--- pwa/ma7alak-service-worker.js
+++ pwa/ma7alak-service-worker.js
@@ -7,7 +7,7 @@
 */
 "use strict";
 
-const VERSION="m7-pwa-2026-09-20-5";
+const VERSION="m7-pwa-2026-09-20-6";
 const IMMUTABLE_CACHE=VERSION+"-immutable";
 
 self.addEventListener("install",event=>{
